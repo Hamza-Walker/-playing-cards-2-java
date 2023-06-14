87c8fa38ad807e42558f9542b14c794ac241add5
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-06-14 05:55:55 +0200

    wrote the code for the deck generator

---
+++ src/main/java/com/codecool/service/FrenchCardGenerator.java
@@ -0,0 +1,39 @@
+package com.codecool.service;
+
+import com.codecool.model.Card;
+import com.codecool.model.Suit;
+import com.codecool.model.Symbol;
+
+import java.util.ArrayList;
+
+public class FrenchCardGenerator {
+    private final static ArrayList<Card> deck = new ArrayList<>();
+    public FrenchCardGenerator() {
+        generateNumberedCards();
+        generateCourtCards();
+    }
+
+    public static ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    private void generateNumberedCards () {
+        for (Suit suit : Suit.values()) {
+            for (int number = 2; number <= 10; number++) {
+                Card card = new Card(Symbol.NUMBER(number), suit);
+                deck.add(card);
+            }
+        }
+    }
+    private void generateCourtCards () {
+        for (Suit suit : Suit.values()) {
+            Symbol[] courtSymbols = {Symbol.JACK, Symbol.QUEEN, Symbol.KING, Symbol.ACE};
+
+            for (Symbol courtSymbol : courtSymbols) {
+                Card card = new Card(courtSymbol, suit);
+                deck.add(card);
+            }
+        }
+
+    }
+}
--- src/main/java/com/codecool/service/GenerateCards.java
+++
@@ -1,4 +0,0 @@
-package com.codecool.service;
-
-public class GenerateCards {
-}
